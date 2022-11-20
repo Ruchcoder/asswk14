d4aae8d900b3f68105ad7c9936655f74efff492c
Ruchinna <ruthokeniyi@gmail.com>
2022-11-20 17:05:11 +0100

    sign in page

---
+++ android/app/src/main/kotlin/com/example/asswk14/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asswk14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blue,
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 250, 30, 20),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                //mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(" Smart \n Insurance \n here!",
+                      style: Theme.of(context).textTheme.headline2
+
+                  ),
+
+                  Text("\n Find all your needs faster \nthan ever",
+                    style: TextStyle(
+                           color: Colors.white
+                    ),
+                  ),
+
+                  Text("\n\n\n\nNew Account"),
+
+                  Text("\n\n\nSign In"),
+
+                ],
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
